b92f3704a378067d32ab02b987bd54321d6b713f
Riitta Pessi <riitta@pessi.fi>
2023-11-01 20:52:33 +0200

    Viikon 1 tehtävien välitallennus

---
+++ viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -0,0 +1,50 @@
+import unittest
+from statistics_service import StatisticsService, sort_by_points
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+
+    def test_players_by_team(self):
+        players = self.stats.team("PIT")
+        self.assertEqual(1, len(players))
+        players = self.stats.team("EDM")
+        self.assertEqual(3, len(players))
+        player = self.stats.team("DET")[0]
+        self.assertEqual(player.name, "Yzerman")
+
+    def test_player_points(self):
+        player = self.stats.team("DET")[0]
+        points = sort_by_points(player)
+        self.assertEqual(points, 98)
+
+    def test_search_works(self):
+        players = self.stats
+        player = players.search("Kurri")
+        self.assertEqual(player.name, "Kurri")
+        player = players.search("Greztky")
+        self.assertEqual(player, None)
+
+    def test_top_ranking_works(self):
+        players = self.stats
+        top_5 = players.top(5)
+        self.assertEqual(len(top_5), 5)
+        self.assertEqual(sort_by_points(top_5[0]), 124)
+
+
+
+
