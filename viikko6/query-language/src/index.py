c9cc2edc378acca6d058eb549354184d34cbb576
Riitta Pessi <riitta@pessi.fi>
2023-12-10 21:22:58 +0200

    Lisätty tehtävät 3-5

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -2,7 +2,7 @@ from rich.console import Console
 from rich.table import Table
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or, QueryBuilder
 
 def main():
     console = Console()
@@ -37,9 +37,32 @@ def main():
     console.print(grid)
     print("\n")
 
-    filtered_with_all = stats.matches(All())
-    console.print(len(filtered_with_all), style = 'purple4')
+    query = QueryBuilder()
+    matcher = query.playsIn("NYR").hasAtLeast(10, "goals").hasFewerThan(20, "goals").build()
 
+    for player in stats.matches(matcher):
+        print(player)
+
+    m1 = (
+        query
+            .playsIn("PHI")
+            .hasAtLeast(10, "assists")
+            .hasFewerThan(5, "goals")
+            .build()
+        )
+
+    m2 = (
+        query
+            .playsIn("EDM")
+            .hasAtLeast(50, "points")
+            .build()
+    )
+    print()
+    query = QueryBuilder()
+    matcher = query.oneOf(m1, m2).build()
+
+    for player in stats.matches(matcher):
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -48,3 +48,34 @@ class HasFewerThan:
     def test(self, player):
         player_value = getattr(player, self._attr)
         return player_value < self._value
+
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
+
+class QueryBuilder:
+    def __init__(self, querystack = All()):
+        self.querystack = querystack
+
+    def build(self):
+        return self.querystack
+
+    def playsIn(self, team):
+        teamplayers = PlaysIn(team)
+        return QueryBuilder(And(self.querystack, teamplayers))
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self.querystack, HasAtLeast(value, attr)))
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self.querystack, HasFewerThan(value, attr)))
+
+    def oneOf(self, query1, query2):
+        return QueryBuilder(Or(query1, query2))
